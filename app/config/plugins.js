89d3a77c0a147108974e36cc2a7df4475a963e03
hiimzimmy <holcmannsimonas@gmail.com>
2022-06-10 19:06:19 +0300

    add microsoft azure plugin

---
+++ app/config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+    upload: {
+      provider: 'azure-storage',
+      providerOptions: {
+        account: env('STORAGE_ACCOUNT'),
+        accountKey: env('STORAGE_ACCOUNT_KEY'),
+        containerName: env('STORAGE_CONTAINER_NAME'),
+        cdnBaseURL: env('STORAGE_CDN_URL'),
+        defaultPath: 'assets',
+        maxConcurrent: 10
+      }
+    }
+  });
